5099883646c960dd891589074e13a66932409bd8
Duke-Pereira <ald59@tmw.com>
2020-04-08 21:38:44 -0700

    Some simple clean up and animation for top prop in prep for some more functionality

--- src/Components/ContainerTitle.js
+++ src/Components/ContainerTitle.js
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const ContainerTitle = () => (
-  <div className='container-title'>
-    <h1>Share the process</h1>
-    <div className='title-design-element' />
-  </div>
-)
+const ContainerTitle = () => {
+  const [classList, setClassList] = useState('title-design-element')
+
+  const handleAddClass = () => {
+    if (classList === 'title-design-element' || classList === 'title-design-element title-design-element-right') {
+      setClassList('title-design-element title-design-element-left')
+    } else {
+      setClassList('title-design-element title-design-element-right')
+    }
+  }
+
+  return (
+    <div className='container-title'>
+      <h1>Share the process</h1>
+      <div className={classList} onClick={handleAddClass} />
+    </div>
+  )
+}
 
 export default ContainerTitle
